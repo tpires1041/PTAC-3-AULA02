513ecbc793162f47aeb46c8dbc3970d5d8eb6cdd
tpires1041 <tiagopires2501@outlook.com>
2024-03-06 09:23:29 -0400

    Correcoes
    pequenas correcoes

--- src/app/components/Map.js
+++ src/app/components/Map.js
@@ -1,11 +1,11 @@
-export default function Footer(){
+import styles from "./map.module.css";
+
+export default function Map(){
     return(
-        <iframe
+        <iframe className={styles.iframe}
           id="gmap_canvas"
           src={"https://maps.google.com/maps?       q=Nova%20Andradina%20+IFMS+&t=&z=13&ie=UTF8&iwloc=&output=embed"}
-          frameborder="0" scrolling="no"
-          marginheight="0"
-          marginwidth="0">
+          frameborder="0" scrolling="no">
 </iframe>
     )
 }
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,12 @@
 import Link from "next/link"
 import Menu from "./components/Menu"; 
 import Footer from "./components/Footer";
+import styles from "./components/not-found.module.css"
 
 export default function NotFound(){
     return (
         <div>
-        <h2>A página não foi encontrada</h2>
+        <h2 className={styles.h2}>A página não foi encontrada</h2>
         <Link href="/">Retornar para Home</Link>
         </div>
     )
