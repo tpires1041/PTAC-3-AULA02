5a516420d89bff9b3505e6050b5f27c85dea5558
tpires1041 <tiagopires2501@outlook.com>
2024-03-20 17:27:25 -0400

    commit 20 03 24
    servidor

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default async function Home() {
-  const resposta = await fetch("http://localhost:3000/api", {
+  const resposta = await fetch("https://back-end-ifms-blond.vercel.app/campi", {
     next: { revalidate: 1 },
   });
 
